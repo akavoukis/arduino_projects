dd8ed02fe836b4fa1986d2f2c60b9cdff4057990
akavoukis <tasos.kavoukis@gmail.com>
2017-02-22 23:06:24 +0000

    added button delay

--- tuner_remote/tuner_remote.ino
+++ tuner_remote/tuner_remote.ino
@@ -14,161 +14,152 @@ int RECV_PIN = 10;
 IRrecv irrecv(RECV_PIN);
 
 decode_results results;
+int last_received;
+int same_counter=0;
 
 void setup()
 {
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
   Keyboard.begin();
-
 }
 
 void loop() {
   if (irrecv.decode(&results)) 
-  {
-    switch (results.value)
-    {
-      // button 1
-      case 0x1CF:
-      case 0x9CF:
-        break;
-            
-      // button 2
-      case 0x1D5:
-      case 0x9D5:
-        Keyboard.press(KEY_UP_ARROW);
-        break;
-
-      // button 3
-      case 0x1D0:
-      case 0x9D0:
-        break;
+  { 
+    results.value = results.value & 0xFF;
+    if (last_received == results.value) 
+      same_counter++ ;
+    else
+      same_counter=0;
       
-      // button 4
-      case 0x1D8:
-      case 0x9D8:
-        Keyboard.press(KEY_LEFT_ARROW);
-        break;   
-
-      // button 5
-      case 0x1DB:
-      case 0x9DB:
-        Keyboard.press(KEY_RETURN);
-        break;  
-
-      // button 6
-      case 0x1DE:
-      case 0x9DE:
-        Keyboard.press(KEY_RIGHT_ARROW);
-        break;  
-
-      // button 7
-      case 0x1D1:
-      case 0x9D1:
-        Keyboard.press(KEY_BACKSPACE);
-        break; 
-
-      // button 8
-      case 0x1E1:
-      case 0x9E1:
-        Keyboard.press(KEY_DOWN_ARROW);
-        break; 
+    if ((same_counter > 4) || (last_received != results.value))
+    {
+      switch (results.value)
+      {
+        // button 1
+        case 0xCF:
+          break;
+              
+        // button 2
+        case 0xD5:
+          Keyboard.press(KEY_UP_ARROW);
+          break;
+  
+        // button 3
+        case 0xD0:
+          break;
         
-      // button 9
-      case 0x1D2:
-      case 0x9D2:
-        break;  
-
-      // source menu
-      case 0x1E4:
-      case 0x9E4:
-        // open context menu
-        Keyboard.press('C');      
-        break; 
-         
-      // button 0
-      case 0x1E7:
-      case 0x9E7:
-        break; 
-
-      // button T
-      case 0x1EA:
-      case 0x9EA:
-        break; 
-        
-      // button Mute
-      case 0x1C0:
-      case 0x9C0:
-        Keyboard.press(KEY_F8);
-        break; 
-
-      // button pinnacle
-      case 0x1C1:
-      case 0x9C1:
-        Keyboard.press('H');
-        break; 
-
-      // button volume up
-      case 0x1C3:
-      case 0x9C3:
-        Keyboard.press('+');
-        break;
-
-      // button volume down
-      case 0x1C9:
-      case 0x9C9:
-        Keyboard.press('-');
-        break;
-
-      // button program up
-      case 0x1C6:
-      case 0x9C6:
-        Keyboard.press(KEY_UP_ARROW);
-        break;
-
-      // button program down
-      case 0x1CC:
-      case 0x9CC:
-        Keyboard.press(KEY_DOWN_ARROW);
-        break;
-
-      // button rewind
-      case 0x1ED:
-      case 0x9ED:
-        break;
-
-      // button forward
-      case 0x1F3:
-      case 0x9F3:
-        break;
-
-      // button play pause
-      case 0x1F0:
-      case 0x9F0:
-        Keyboard.press(' ');
-        break;
-
-      // button record
-      case 0x1F6:
-      case 0x9F6:
-        Keyboard.press('K');
-        break;
-
-      // button stop
-      case 0x1FC:
-      case 0x9FC:
-        Keyboard.press('X');
-        break;
-
-      // button help
-      case 0x1FF:
-      case 0x9FF:
-        Keyboard.press('O');
-        break;
+        // button 4
+        case 0xD8:
+          Keyboard.press(KEY_LEFT_ARROW);
+          break;   
+  
+        // button 5
+        case 0xDB:
+          Keyboard.press(KEY_RETURN);
+          break;  
+  
+        // button 6
+        case 0xDE:
+          Keyboard.press(KEY_RIGHT_ARROW);
+          break;  
+  
+        // button 7
+        case 0xD1:
+          Keyboard.press(KEY_BACKSPACE);
+          break; 
+  
+        // button 8
+        case 0xE1:
+          Keyboard.press(KEY_DOWN_ARROW);
+          break; 
+          
+        // button 9
+        case 0xD2:
+          break;  
+  
+        // source menu
+        case 0xE4:
+          // open context menu
+          Keyboard.press('C');      
+          break; 
+           
+        // button 0
+        case 0xE7:
+          break; 
+  
+        // button T
+        case 0xEA:
+            Keyboard.press('T');
+          break; 
+          
+        // button Mute
+        case 0xC0:
+          Keyboard.press(KEY_F8);
+          break; 
+  
+        // button pinnacle
+        case 0xC1:
+          Keyboard.press('e');
+          break; 
+  
+        // button volume up
+        case 0xC3:
+          Keyboard.press('+');
+          break;
+  
+        // button volume down
+        case 0xC9:
+          Keyboard.press('- ');
+          break;
+  
+        // button program up
+        case 0xC6:
+          Keyboard.press(KEY_UP_ARROW);
+          break;
+  
+        // button program down
+        case 0xCC:
+          Keyboard.press(KEY_DOWN_ARROW);
+          break;
+  
+        // button rewind
+        case 0xED:
+          Keyboard.press(KEY_LEFT_CTRL);
+          Keyboard.press(KEY_LEFT_ARROW);
+          break;
+  
+        // button forward
+        case 0xF3:
+          Keyboard.press(KEY_LEFT_CTRL);
+          Keyboard.press(KEY_RIGHT_ARROW);
+          break;
+  
+        // button play pause
+        case 0xF0:
+          Keyboard.press(' ');
+          break;
+  
+        // button record
+        case 0xF6:
+          Keyboard.press('K');
+          break;
+  
+        // button stop
+        case 0xFC:
+          Keyboard.press('X');
+          break;
+  
+        // button help
+        case 0xFF:
+          Keyboard.press('O');
+          break;
+      }
+      Keyboard.releaseAll();
     }
-    Keyboard.releaseAll();
+    last_received = results.value;
     Serial.println(results.value, HEX);
     irrecv.resume(); // Receive the next value
   }
-delay(100);
 }
